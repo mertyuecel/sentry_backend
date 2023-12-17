27e8f41ba38107e96de67272e197995b17667fd2
Mert Yuecel <mert.yuecel@novatec-gmbh.de>
2023-12-17 22:17:31 +0100

    New configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.1.5"
 	id("io.spring.dependency-management") version "1.1.3"
-	id ("io.sentry.jvm.gradle") version "3.14.0"
+	id("io.sentry.jvm.gradle") version "4.0.0"
 }
 
 group = "de.novatec"
@@ -22,23 +22,25 @@ repositories {
 	mavenCentral()
 }
 
-extra["sentryVersion"] = "6.28.0"
+sentry {
+	// Generates a JVM (Java, Kotlin, etc.) source bundle and uploads your source code to Sentry.
+	// This enables source context, allowing you to see your source
+	// code as part of your stack traces in Sentry.
+	includeSourceContext = true
+
+	org = "none-ruy"
+	projectName = "spring-sentry-demo"
+	authToken = System.getenv("SENTRY_AUTH_TOKEN=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")
+}
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("io.sentry:sentry-spring-boot-starter-jakarta")
 
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("org.postgresql:postgresql")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
-
-dependencyManagement {
-	imports {
-		mavenBom("io.sentry:sentry-bom:${property("sentryVersion")}")
-	}
 }
 
 tasks.withType<Test> {
